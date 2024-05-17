c82264ce5eb1c3d971274278ad4c4633a5b3f618
Riteshk_007 <codeshorts007@gmail.com>
2024-05-17 17:06:23 +0530

    refactor: Update service worker registration code
    Simplify and update the service worker registration code in index.html and sw.js files to improve readability and maintainability.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "Spiderman-Game-Cache-v1";
-const urlsToCache = [
+const cacheName = "spiderman-game";
+const filesToCache = [
   "/",
   "/index.html",
   "/styles.css",
@@ -8,37 +8,18 @@ const urlsToCache = [
   "/villen.png",
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
     })
   );
 });
